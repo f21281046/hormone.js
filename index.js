2fb987203a3e8531d9bcab79f2943098a532a08d
qqwq3 <leehormone@yeah.net>
2019-02-27 14:55:01 +0800

    是

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  * */
 
 export {default as analysisIP} from './src/analysisIp';
-export {default as arrayStructured} from './src/arrayStructured';
 export {default as typeCheck} from './src/typeCheck';
 export {default as checkEmpty} from './src/checkEmpty';
 export {default as currency} from './src/currency';
